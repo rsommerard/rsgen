0d3a5eeb483350d551bc0bac52b04514de9d6eeb
Romain SOMMERARD <romain.sommerard@gmail.com>
2014-09-10 21:35:58 +0200

    HTML file
    Basic HTML file generator added.

--- rsgen.rb
+++ rsgen.rb
@@ -1,12 +1,53 @@
 require 'thor'
 
+DEBUG = true
+
 class RSGen < Thor
-  option :empty, :type => :boolean, :aliases => "-e"
 
   desc "html [filename]", "create a new html file"
+  option :empty, :type => :boolean, :aliases => "-e"
   def html(filename = 'index')
-    puts "new html file: #{filename}"
-    puts "with empty option" if options[:empty]
+    if options[:empty]
+      create_html_file(filename)
+      return
+    end
+
+    content = "<!DOCTYPE>\n"
+    content += "<html>\n"
+    content += "  <head>\n"
+    content += "    <meta charset='UTF-8'>\n"
+    content += "    <title>#{ filename }</title>\n"
+    content += "  </head>\n"
+    content += "  <body>\n"
+    content += "  </body>\n"
+    content += "</html>\n"
+
+    create_html_file(filename, content)
+  end
+
+  private
+
+  def create_html_file(filename, content = nil)
+    path = generate_path(filename, 'html')
+    create_file(path, content)
+  end
+
+  def create_file(path, content)
+    File.open(path, 'w') { |file| file.write(content) }
+    puts "#{ path } created."
+  end
+
+  def generate_path(filename, extension)
+    path = "#{ filename }.#{ extension }"
+
+    i = 1
+    while File.exist?(path) do
+      puts "#{ path } already exist."
+      path = "#{ filename }" + i.to_s + ".#{ extension }"
+      i += 1
+    end
+
+    return path
   end
 end
 
